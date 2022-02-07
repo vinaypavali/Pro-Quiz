84c948aa624fc25913a1c71db8f66a30a7bfb31a
Vinay Kumar <pavalivinay6@gmail.com>
2022-02-08 00:18:29 +0530

    changes in QuizActivity

--- app/src/main/java/com/quiz/proquiz/QuizActivity.java
+++ app/src/main/java/com/quiz/proquiz/QuizActivity.java
@@ -1,27 +1,34 @@
 package com.quiz.proquiz;
 
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.database.FirebaseDatabase;
+import com.quiz.proquiz.Auth.LoginActivity;
 
 import java.util.ArrayList;
+import java.util.Locale;
+import java.util.Random;
 
 public class QuizActivity extends AppCompatActivity {
 
-    ArrayList<Questions> questions;
-    int index=0;
-    Questions question;
     CountDownTimer timer;
     FirebaseDatabase database;
-    int timeValue =25;
-    int correctAnswers = 0;
+    int currentPos=0, correctCount = 0,timeValue =30,wrongCount=0;
     TextView qc,tc,mq,o1,o2,o3,o4;
+    Button quit,next;
+    ArrayList<Questions> questionsArrayList;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -29,48 +36,114 @@ public class QuizActivity extends AppCompatActivity {
         setContentView(R.layout.activity_quiz);
         getSupportActionBar().hide();
 
-        TextView qc = (TextView) findViewById(R.id.questionCounter);
-        TextView tc = (TextView) findViewById(R.id.timer);
+         qc = (TextView) findViewById(R.id.questionCounter);
+         tc = (TextView) findViewById(R.id.timer);
 
-        TextView mq = (TextView) findViewById(R.id.question);
+          mq = (TextView) findViewById(R.id.question);
 
-        TextView o1 = (TextView) findViewById(R.id.option_1);
-        TextView o2 = (TextView) findViewById(R.id.option_2);
-        TextView o3 = (TextView) findViewById(R.id.option_3);
-        TextView o4 = (TextView) findViewById(R.id.option_4);
+          o1 = (TextView) findViewById(R.id.option_1);
+          o2 = (TextView) findViewById(R.id.option_2);
+          o3 = (TextView) findViewById(R.id.option_3);
+          o4 = (TextView) findViewById(R.id.option_4);
 
-        Button quit = (Button) findViewById(R.id.quitBtn);
-        Button next = (Button) findViewById(R.id.nextBtn);
+         quit = (Button) findViewById(R.id.quitBtn);
+         next = (Button) findViewById(R.id.nextBtn);
 
-        questions = new ArrayList<>();
+        questionsArrayList = new ArrayList<>();
 
-        questions.add(new Questions("What is earth ?",
+
+        quit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(QuizActivity.this, MainActivity.class));
+
+            }
+        });
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(currentPos<questionsArrayList.size()) {
+                    currentPos++;
+                    setNextQuestion();
+                }else{
+                    gameWon();
+                    Toast.makeText(QuizActivity.this,"Quiz Finished",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+        getQuestions(questionsArrayList);
+        resetTimer();
+        setNextQuestion();
+
+    }
+
+    private void getQuestions(ArrayList<Questions> questionsArrayList) {
+
+        questionsArrayList.add(new Questions("What is earth ?",
                 "Planet","Sun","Human","Ball","Planet"));
-        questions.add(new Questions("What is Sun ?",
+        questionsArrayList.add(new Questions("What is Sun ?",
                 "Star","Planet","Human","ball","Star"));
 
-        timer = new CountDownTimer(25000,1000) {
-            @Override
-            public void onTick(long l) {
-                timeValue = timeValue-1;
+    }
 
+    public void setNextQuestion() {
+        if(timer != null)
+            timer.cancel();
+        resetTimer();
+        timer.start();
+        if(currentPos<questionsArrayList.size()){
+            qc.setText(String.format("%d/%d",(currentPos+1),questionsArrayList.size()));
+            mq.setText(questionsArrayList.get(currentPos).getQuestion());
+            o1.setText(questionsArrayList.get(currentPos).getOption1());
+            o2.setText(questionsArrayList.get(currentPos).getOption2());
+            o3.setText(questionsArrayList.get(currentPos).getOption3());
+            o4.setText(questionsArrayList.get(currentPos).getOption4());
+        }
+
+    }
+    void resetTimer() {
+        timer = new CountDownTimer(30000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                tc.setText(String.valueOf(millisUntilFinished / 1000));
 
             }
 
             @Override
             public void onFinish() {
+                Dialog dialog=new Dialog(QuizActivity.this);
+                dialog.getWindow().addFlags(WindowManager.LayoutParams.FLAG_BLUR_BEHIND);
+                dialog.setContentView(R.layout.time_out);
+                dialog.findViewById(R.id.tryBtn).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        startActivity(new Intent(QuizActivity.this, MainActivity.class));
+                    }
+                });
+                dialog.show();
 
             }
-        };
+        }.start();
 
     }
-    public void setNextQuestion() {
 
-        if(index < questions.size()){
-            Questions question = questions.get(index);
+    public void correct(){
+        correctCount++;
+        currentPos++;
 
-        }
+    }
+    public void wrong(){
+        wrongCount++;
+        currentPos++;
+
     }
 
+    private void gameWon(){
+        startActivity(new Intent(QuizActivity.this,ResultActivity.class));
+
+    }
 
 }
